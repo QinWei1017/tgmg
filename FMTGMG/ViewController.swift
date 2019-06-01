de5acc17996de1cf5708424b233bbb0b2519487d
匀视网Mac <yunshiwangmac@yunshiwangMacdeMac-mini.local>
2019-06-01 14:18:45 +0800

    初次提交

---
+++ FMTGMG/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FMTGMG
+//
+//  Created by 匀视网Mac on 2019/6/1.
+//  Copyright © 2019 匀视网Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
